c98e784270c6b49779a02304a9843eb60e9a1a7c
nkalpak <nikita.kalpakovski@outlook.com>
2023-01-04 13:12:21 +0100

    prototype

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -7,6 +7,9 @@ import { z } from "zod";
  */
 export const serverSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]),
+  AWS_ACCESS_KEY_ID: z.string(),
+  AWS_SECRET_ACCESS_KEY: z.string(),
+  AWS_BUCKET_NAME: z.string()
 });
 
 /**
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,6 +1,9 @@
 import { z } from "zod";
-
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { createTRPCRouter, publicProcedure } from "../trpc";
+import { s3 } from "../../lib/aws";
+import { env } from "../../../env/server.mjs";
+import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
@@ -9,5 +12,48 @@ export const exampleRouter = createTRPCRouter({
       return {
         greeting: `Hello ${input.text}`,
       };
+    }),
+
+  uploadImage: publicProcedure
+    .input(z.array(z.string()))
+    .mutation(async ({ input }) => {
+      return Promise.all(
+        input.map((name) =>
+          getSignedUrl(
+            s3,
+            new PutObjectCommand({
+              Bucket: env.AWS_BUCKET_NAME,
+              Key: name,
+            }),
+            {
+              expiresIn: 3600,
+            }
+          )
+        )
+      );
+    }),
+
+  getImages: publicProcedure
+    .input(z.array(z.string()))
+    .query(async ({ input }) => {
+      const result = await Promise.all(
+        input.map((name) =>
+          getSignedUrl(
+            s3,
+            new GetObjectCommand({
+              Bucket: env.AWS_BUCKET_NAME,
+              Key: name,
+            }),
+            {
+              expiresIn: 3600,
+            }
+          )
+        )
+      );
+
+      return result.map((url, index) => ({
+        name: input[index]!,
+        url,
+      }));
     }),
 });
